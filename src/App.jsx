db6d403490f0994b5f9214bc7549863f93ce99f7
danilloWillian <danwillgon@gmail.com>
2023-10-26 14:54:38 -0300

    update curso celke props

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import Header from "./components/Header";
+import Comment from "./components/Comment";
 
 function App() {
+
+  const comment = {
+    date: new Date(),
+    text:'Não faça nada em troca ou pensando em colher frutos , apenas faça o bem ',
+    author: {
+      name:'Willian',
+      avatarUrl:'https://placekitten.com/g/64/64'
+    }
+  }
   return (
     <div >
-      <h1> Ola <Welcome name="Mariana"/> </h1>
+     <Header />
+     <Comment  date={comment.date}
+     text={comment.text} 
+     author={comment.author}
+      />
+  
     </div>
+ 
   );
-}
-
-/*function Welcome(props){
-  return <span>{props.name}</span>
-}*/
-
-class Welcome extends React.Component{
-  render(){
-    return <span>{this.props.name} </span>
-  }
 }
 
 export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div><header>Cabeçalho</header>
+        </div>
+            )
+
+}
+
+            export default Header;
