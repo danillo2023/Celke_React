8daf4b312c7dbcea2a1f94e2f39c136b91727810
danilloWillian <danwillgon@gmail.com>
2023-10-26 08:27:41 -0300

    update curso celke react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
 function App() {
-
   return (
     <div >
-      <h1> Ola <Welcome name="Cesar"/> </h1>
+      <h1> Ola <Welcome name="Mariana"/> </h1>
     </div>
   );
 }
 
-function Welcome(props){
+/*function Welcome(props){
   return <span>{props.name}</span>
+}*/
+
+class Welcome extends React.Component{
+  render(){
+    return <span>{this.props.name} </span>
+  }
 }
 
 export default App;
